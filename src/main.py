9e501a3ed62cd2018953aee0d2555fd8214e0e1a
Валентин Вольфович <96646811+Loliimilk@users.noreply.github.com>
2026-04-28 12:31:42 +1000

    feat: добавлена подготовка API и модели
    - импортируется ключ и API
    - проверяет есть ли ключ и API
    - переменные для входящих и выходящих отзывов и ссылка на API в openrouter

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 import os
+import json
+import re
 import requests
 import pandas as pd
 from dotenv import load_dotenv
 import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+MODEL = os.getenv("MODEL")
+
+if not API_KEY:
+    raise ValueError("API_KEY не найден в .env")
+
+if not MODEL:
+    raise ValueError("MODEL не найдена в .env")
+
+INPUT_FILE = "data/input_reviews.csv"
+OUTPUT_FILE = "data/output_reviews.csv"
+API_URL = "https://openrouter.ai/api/v1/chat/completions"
